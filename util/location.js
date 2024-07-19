0ee7ef454f72020bcc29f64b350d87a69519bd6d
Meet Harsoda <meetharsoda5@gmail.com>
2024-07-19 11:37:41 +0530

    Added-Ola-Maps

--- util/location.js
+++ util/location.js
@@ -1,38 +1,30 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const axios = require('axios');
+const axios = require('axios');
 
-// const HttpError = require('../models/http-error');
+const HttpError = require('../models/http-error');
 
-// const API_KEY = process.env.GOOGLE_API_KEY;
+const API_KEY = process.env.OLA_API_KEY;
 
 async function getCoordsForAddress(address) {
-  return {
-    lat: 40.7484474,
-    lng: -73.9871516
-  };
-  // const response = await axios.get(
-  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-  //     address
-  //   )}&key=${API_KEY}`
-  // );
+  const response = await axios.get(
+    `https://api.olamaps.io/places/v1/geocode?address=${encodeURIComponent(
+      address
+    )}&language=English&api_key=${API_KEY}`
+  );
 
-  // const data = response.data;
+  const data = response.data;
 
-  // if (!data || data.status === 'ZERO_RESULTS') {
-  //   const error = new HttpError(
-  //     'Could not find location for the specified address.',
-  //     422
-  //   );
-  //   throw error;
-  // }
+  if (!data || data.status === 'zero_results') {
+    const error = new HttpError(
+      'Could not find location for the specified address. Please enter a valid address.',
+      422
+    );
+    throw error;
+  }
 
-  // const coordinates = data.results[0].geometry.location;
-
-  // return coordinates;
+  const coordinates = data.geocodingResults[0].geometry.location;
+  return coordinates;
 }
 
 module.exports = getCoordsForAddress;
-
-
-//commented code requires google api key
